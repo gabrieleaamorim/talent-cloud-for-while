6d3071a3da381f6cf39ca94458d4dc4d4b0e420f
Gabriele Amorim <gabriele.amorim35@gmail.com>
2024-02-24 13:49:17 -0300

    first commit

---
+++ pratica_lacos.py
@@ -0,0 +1,21 @@
+print('Atividade no Laço For')
+
+for i in range(20, 0 , -1):
+    if(i != 13):
+        print(i)
+
+
+print('Atividade no Laço While')
+
+i = 20
+while i > 0:
+    if(i != 13):
+        print(i)
+    i = i - 1
+    
+
+
+    
+
+
+# for i in range(20, 0):
